1cee4a54ee52055bfc05de68b4b439d90174bbb5
cynamonized <64099194+cynamonized@users.noreply.github.com>
2023-07-29 19:27:02 +0200

    Added FAKE DOC functionality to the server and added proper conditions to the API request functions

--- js/APICommunication/getUserAppoinments.js
+++ js/APICommunication/getUserAppoinments.js
@@ -20,32 +20,40 @@ export const getUserAppointmentsMultiArray = async (
       )
     );
     let multiArray = [];
-    let firstArray = [];
-    let secondArray = [];
+    let firstArray = fillApposArray(bookedAppointments);
+    let secondArray = fillApposArray(completedApointments);
 
-    // Need to sort data here by date
+    ////////////////////////////////////////////////////////
+    // bookedAppointments.forEach((appo) => {
+    //   const appoWithID = appo.data();
+    //   appoWithID["id"] = appo.id;
+    //   firstArray.push(appoWithID);
+    // });
 
-    bookedAppointments.forEach((appo) => {
-      const appoWithID = appo.data();
-      appoWithID["id"] = appo.id;
+    // console.log(firstArray);
 
-      firstArray.push(appoWithID);
-    });
-    firstArray.sort((a, b) => {
-      return a.date.seconds - b.date.seconds;
-    });
+    // // Need to sort data here by date
+    // if (firstArray.length > 0) {
+    //   firstArray.sort((a, b) => {
+    //     return a.date.seconds - b.date.seconds;
+    //   });
+    // }
+    //////////////////////////////////////////////////////////
 
-    // Need to sort data here by date
+    // completedApointments.forEach((appo) => {
+    //   const appoWithID = appo.data();
+    //   appoWithID["id"] = appo.id;
+    //   secondArray.push(appoWithID);
+    // });
 
-    completedApointments.forEach((appo) => {
-      const appoWithID = appo.data();
-      appoWithID["id"] = appo.id;
+    // // Need to sort data here by date
+    // if (secondArray.length > 0) {
+    //   secondArray.sort((a, b) => {
+    //     return a.date.seconds - b.date.seconds;
+    //   });
+    // }
 
-      secondArray.push(appoWithID);
-    });
-    secondArray.sort((a, b) => {
-      return a.date.seconds - b.date.seconds;
-    });
+    //////////////////////////////////////////////////////////
 
     multiArray.push(firstArray);
     multiArray.push(secondArray);
@@ -54,4 +62,26 @@ export const getUserAppointmentsMultiArray = async (
   } catch (error) {
     console.log(error);
   }
+};
+
+const fillApposArray = (entryApposArray) => {
+  let outputArray = [];
+
+  entryApposArray.forEach((appo) => {
+    const appoWithID = appo.data();
+    appoWithID["id"] = appo.id;
+
+    if (appoWithID.id != "FAKE DOC") {
+      outputArray.push(appoWithID);
+    }
+  });
+
+  // Sort data by date if there are any appos
+  if (outputArray.length > 0) {
+    outputArray.sort((a, b) => {
+      return a.date.seconds - b.date.seconds;
+    });
+  }
+
+  return outputArray;
 };
