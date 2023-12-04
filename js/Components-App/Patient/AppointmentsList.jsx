fa2f5c9bcdc2909c2c57fd9ccaa6be165e27d2d4
cynamonized <64099194+cynamonized@users.noreply.github.com>
2023-12-04 23:26:35 +0100

    Adding RWD to tables - Appo list WIP

--- js/Components-App/Patient/AppointmentsList.jsx
+++ js/Components-App/Patient/AppointmentsList.jsx
@@ -6,6 +6,7 @@ import {
   AppoTimeFromSeconds,
 } from "../../Functions/convertTime";
 import { getUserAppointmentsMultiArray } from "../../APICommunication/getUserAppoinments";
+import { useWindowSize } from "@uidotdev/usehooks";
 
 export const AppointmentsList = ({ currentUserUID }) => {
   const [appoMultiArray, setAppoMultiArray] = useState(null);
@@ -38,71 +39,113 @@ export const AppointmentsList = ({ currentUserUID }) => {
 };
 
 const AppointetsTable = ({ title, appos, isCompleted }) => {
+  const size = useWindowSize();
+
   return (
     <>
       <div className="body__table dashboard-table appo-list__table--my-appos">
         <p className="table__title">{title}</p>
-        <table className="table__content">
-          <thead>
-            <tr className="content__row-head">
-              <th className=" col-date">Date</th>
-              <th className=" col-time">Hour</th>
-              <th className=" col-doctor">Doctor</th>
-              <th className=" col-spec">Specialization</th>
-              <th className=" col-address">Localization</th>
-              <th className=" col-set content__row-head--last">Settings</th>
-            </tr>
-          </thead>
-          <tbody>
-            {appos &&
-              appos.map((appo) => {
-                if (!appo.specialization) {
-                  return "";
-                }
-                return (
-                  <tr
-                    key={appo.id}
-                    className={
-                      isCompleted == true
-                        ? "appo-row appo-greyed-out"
-                        : "appo-row"
-                    }
-                  >
-                    <td className=" col-date">
-                      {AppoPureDateFromSeconds(appo.date.seconds)}
-                    </td>
-                    <td className=" col-time">
-                      {AppoTimeFromSeconds(appo.date.seconds)}
-                    </td>
-                    <td className=" col-doctor">{appo.doctor}</td>
-                    <td className=" col-spec">{appo.specialization}</td>
-                    <td className=" col-address">{appo.place}</td>
-
-                    {/* BELOW WORKS, FINALIZE RWD */}
-                    {/* Any way to make it in CSS? IDK REALLY */}
-
-                    <td
-                      className=" col-set content__settings-column"
-                      style={{ marginRight: appos.length < 4 ? "10px" : "0px" }}
+        {size.width > 1005 ? (
+          <table className="table__content">
+            <thead>
+              <tr className="content__row-head">
+                <th className=" col-date">Date</th>
+                <th className=" col-time">Hour</th>
+                <th className=" col-doctor">Doctor</th>
+                <th className=" col-spec">Specialization</th>
+                <th className=" col-address">Localization</th>
+                <th className=" col-set content__row-head--last">Settings</th>
+              </tr>
+            </thead>
+            <tbody>
+              {appos &&
+                appos.map((appo) => {
+                  if (!appo.specialization) {
+                    return "";
+                  }
+                  return (
+                    <tr
+                      key={appo.id}
+                      className={
+                        isCompleted == true
+                          ? "appo-row appo-greyed-out"
+                          : "appo-row"
+                      }
                     >
-                      <Link
-                        to="/single-apointment"
-                        state={{
-                          chosenAppoID: appo.id,
-                          from: "/app-list",
-                          ifCompleted: appo.completed,
+                      <td className=" col-date">
+                        {AppoPureDateFromSeconds(appo.date.seconds)}
+                      </td>
+                      <td className=" col-time">
+                        {AppoTimeFromSeconds(appo.date.seconds)}
+                      </td>
+                      <td className=" col-doctor">{appo.doctor}</td>
+                      <td className=" col-spec">{appo.specialization}</td>
+                      <td className=" col-address">{appo.place}</td>
+
+                      <td
+                        className=" col-set content__settings-column"
+                        style={{
+                          marginRight: appos.length < 4 ? "10px" : "0px",
                         }}
                       >
-                        <span className="material-icons settings-column__icon">
-                          more_horiz
-                        </span>
-                      </Link>
-                    </td>
-                  </tr>
+                        <Link
+                          to="/single-apointment"
+                          state={{
+                            chosenAppoID: appo.id,
+                            from: "/app-list",
+                            ifCompleted: appo.completed,
+                          }}
+                        >
+                          <span className="material-icons settings-column__icon">
+                            more_horiz
+                          </span>
+                        </Link>
+                      </td>
+                    </tr>
+                  );
+                })}
+            </tbody>
+          </table>
+        ) : (
+          <div className="table__content--mobile">
+            {appos &&
+              appos.map((appo) => {
+                return (
+                  <div className="content__single-appo" key={appo.id}>
+                    {/* ADD STYLING TO ALL OF THIS */}
+                    {/* STYLING ON RHS FILE */}
+                    <div className="single-appo__top-row">
+                      <p className="top-row__spec">{appo.specialization}</p>
+                      <div className="top-row__set">
+                        <Link
+                          to="/single-apointment"
+                          state={{
+                            chosenAppoID: appo.id,
+                            from: "/app-list",
+                            ifCompleted: appo.completed,
+                          }}
+                        >
+                          <span className="material-icons settings-column__icon">
+                            more_horiz
+                          </span>
+                        </Link>
+                      </div>
+                    </div>
+
+                    <p className="single-appo__date">
+                      {" "}
+                      {AppoPureDateFromSeconds(appo.date.seconds)}
+                    </p>
+                    <p className="single-appo__time">
+                      {AppoTimeFromSeconds(appo.date.seconds)}
+                    </p>
+                    <p className="single-appo__doctor">{appo.doctor}</p>
+                    <p className="single-appo__address">{appo.place}</p>
+                  </div>
                 );
               })}
-          </tbody>
-        </table>
+          </div>
+        )}
       </div>
     </>
   );
