0d0486a28a97992a32d755b44faa7c1c05430699
cynamonized <64099194+cynamonized@users.noreply.github.com>
2024-01-10 23:55:55 +0100

    Companies WiP

---
+++ js/Components-App/LandingPage/Companies.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+import Logo1 from "../../../images/Landing Page/logos/Logo 1.svg";
+import Logo2 from "../../../images/Landing Page/logos/Logo 2.svg";
+import Logo3 from "../../../images/Landing Page/logos/Logo 3.svg";
+import Logo4 from "../../../images/Landing Page/logos/Logo 4.svg";
+import Logo5 from "../../../images/Landing Page/logos/Logo 5.svg";
+
+export const Companies = () => {
+  return (
+    <div className="companies container">
+      <p className="companies__title">COMPANIES THAT TRUST US</p>
+      <div className="companies__logos"></div>
+    </div>
+  );
+};
--- js/Components-App/LandingPage/LandingPage.jsx
+++ js/Components-App/LandingPage/LandingPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Footer } from "../Footer";
 import { HeroMerged } from "./HeroMerged";
+import { Companies } from "./Companies";
 
 // links in hooks in patient portal are now broken (/single-appointment/  --> /portal/)
 
@@ -8,6 +9,7 @@ export const LandingPage = () => {
   return (
     <>
       <HeroMerged />
+      <Companies />
       <Footer />
     </>
   );
